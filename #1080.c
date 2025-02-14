6b830d2d96e85c7bf2f375c8c39f9b62fea82088
JahdulIslam <ovi242-35-019@diu.edu.bd>
2025-02-14 12:00:54 +0600

    Create #1080.c

---
+++ #1080.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void average();
+
+int main(){
+    int i, x, highest = 0, position;
+
+    for(i = 1; i <= 100; i++){
+        scanf("%d", &x);
+        if(highest < x){
+            highest = x;
+            position = i;
+        }
+    }
+    printf("%d\n", highest);
+    printf("%d\n", position);
+    return 0;
+}
