6b72860fd072f10801b14fa3000874c0bed1fff0
JahdulIslam <ovi242-35-019@diu.edu.bd>
2025-02-22 00:54:08 +0600

    Create #1133.c

---
+++ #1133.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+   int num1, num2, i;
+   scanf("%d %d", &num1, &num2);
+   if(num1 < num2){
+    for(i = num1 + 1; i < num2; i++){
+        if(i%5 == 2 || i%5 == 3){
+            printf("%d\n", i);
+        }
+    }
+   }else{
+    for(i = num2 + 1; i < num1; i++){
+        if(i%5 == 2 || i%5 == 3){
+            printf("%d\n", i);
+        }
+    }
+   }
+   return 0;
+}
